6290783ee148a319e2890606f890538e5e909bba
luxeivan <luxeivan@gmail.com>
2022-10-12 10:50:42 +0300

    commit2

--- src/pages/Successorder.js
+++ src/pages/Successorder.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { sendIsOkFalse } from "../store/cartSlice";
 
 export default function Successorder() {
+  const cart = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(sendIsOkFalse());
+  }, []);
   return (
     <div className="order_success">
       <h3 className="order_success_text">Заказ успешно совершен! Ожидайте звонка оператора</h3>
--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,68 +1,65 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import { redirect, useNavigate } from 'react-router'
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { redirect, useNavigate } from "react-router";
 
-export const fetchCart = createAsyncThunk(
-  'cart/fetchCart',
-  async (order) => {
-    const data = await fetch('http://localhost:7070/api/order',
-      {
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        method: "POST",
-        body: JSON.stringify(order)
-      }).then(responce => responce.status)
-    return data
-  }
-)
+export const fetchCart = createAsyncThunk("cart/fetchCart", async (order) => {
+  const data = await fetch("http://localhost:7070/api/order", {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(order),
+  }).then((responce) => responce.status);
+  return data;
+});
 
 export const cartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     listCart: [],
     isSend: false,
-    sendIsOk: false
+    sendIsOk: false,
   },
   reducers: {
     addCart: (state, action) => {
-      const index = state.listCart.findIndex(item => item.id === action.payload.id)
+      const index = state.listCart.findIndex((item) => item.id === action.payload.id);
       if (index > -1) {
-        state.listCart[index].count += action.payload.count
+        state.listCart[index].count += action.payload.count;
       } else {
-        state.listCart.push(action.payload)
+        state.listCart.push(action.payload);
       }
-      localStorage.listCart = JSON.stringify(state.listCart)
+      localStorage.listCart = JSON.stringify(state.listCart);
     },
     delCart: (state, action) => {
-      state.listCart = state.listCart.filter(item => item.id != action.payload)
-      localStorage.listCart = JSON.stringify(state.listCart)
+      state.listCart = state.listCart.filter((item) => item.id != action.payload);
+      localStorage.listCart = JSON.stringify(state.listCart);
     },
     syncFromLocalStorage: (state, action) => {
       if (localStorage.listCart) {
-        state.listCart = JSON.parse(localStorage.listCart)
+        state.listCart = JSON.parse(localStorage.listCart);
       }
-    }
+    },
+    sendIsOkFalse: (state, action) => {
+      state.sendIsOk = false;
+    },
   },
   extraReducers: {
     [fetchCart.pending]: (state, action) => {
-      state.isSend = true
+      state.isSend = true;
     },
     [fetchCart.fulfilled]: (state, action) => {
       if (action.payload === 204) {
-        state.listCart = []
-        localStorage.listCart = JSON.stringify(state.listCart)
-        state.sendIsOk = true
+        state.listCart = [];
+        localStorage.listCart = JSON.stringify(state.listCart);
+        state.sendIsOk = true;
       }
-      state.isSend = false
+      state.isSend = false;
     },
-    [fetchCart.rejected]: (state, action) => {
-
-    }
-  }
-})
+    [fetchCart.rejected]: (state, action) => {},
+  },
+});
 
 // Action creators are generated for each case reducer function
-export const { addCart, delCart, syncFromLocalStorage } = cartSlice.actions
+export const { addCart, delCart, syncFromLocalStorage,sendIsOkFalse } = cartSlice.actions;
 
-export default cartSlice.reducer
+export default cartSlice.reducer;
